d357b19aa76a3e31c24c277adf4b0cca9116c7ef
Ivan <ivan.sutanto@gmail.com>
2022-03-18 10:43:10 -0700

    Clean up unused imports

--- src/main/java/CalculatorMain.java
+++ src/main/java/CalculatorMain.java
@@ -3,13 +3,7 @@ import com.restaurant365.tools.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.CharBuffer;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class CalculatorMain {
     private final static Logger log = Logger.getLogger(CalculatorMain.class.getName());
