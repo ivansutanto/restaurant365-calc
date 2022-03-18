3f534e1f243112a82cbb239311215b89a09cef1c
Ivan <ivan.sutanto@gmail.com>
2022-03-18 16:45:53 -0700

    Make a friendly exception if something happens

--- src/main/java/CalculatorMain.java
+++ src/main/java/CalculatorMain.java
@@ -1,12 +1,14 @@
 import com.restaurant365.tools.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.logging.Logger;
+
 
 public class CalculatorMain {
-    private final static Logger log = Logger.getLogger(CalculatorMain.class.getName());
+    private final static Logger log = LogManager.getLogger();
 
     public static void main(String[] args) throws IOException {
         // Enter data using BufferReader
@@ -17,7 +19,13 @@ public class CalculatorMain {
     }
 
     public static void calculate(final String inp) {
-        CalculateUtil.calculate(inp);
+        try {
+            CalculateUtil.calculate(inp);
+        } catch (Exception e) {
+            System.out.println("Oops Something may not be correct with your inputs "+inp);
+            log.debug("Something is not right ..."+inp+", Exc:"+e.getMessage());
+        }
+
     }
 
 }
