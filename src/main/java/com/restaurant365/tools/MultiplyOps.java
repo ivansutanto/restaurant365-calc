033cfb7f07ec10f6cbab506b3c3f4ad147a7c7ea
Ivan <ivan.sutanto@gmail.com>
2022-03-18 08:35:25 -0700

    clean up the operation

--- src/main/java/com/restaurant365/tools/MultiplyOps.java
+++ src/main/java/com/restaurant365/tools/MultiplyOps.java
@@ -9,7 +9,7 @@ public class MultiplyOps<T> implements OpsI<T> {
     private static final Logger log = LogManager.getLogger();
 
     private final List<String> numbers;
-    private final ValidationI validationI;
+    private ValidationI validationI;
 
     public MultiplyOps(List<String> arg, ValidationI arg2) {
         this.numbers = arg;
@@ -22,13 +22,14 @@ public class MultiplyOps<T> implements OpsI<T> {
     public T operation() {
         List<Integer> listLong = new ConversionBasicOps(numbers).convert();
         log.debug("Number of Elements:"+listLong.size());
-        ValidationI validationI;
-        if ("N".equals(System.getProperty("ACCEPT_NEG_NUMBER"))) {
-            validationI = new ValidationDenyNegativeNumberInSet(listLong);
-            validationI.validate();
-        }
-        if ("Y".equals(System.getProperty("MAX_NUMBER_2"))) {
-            validationI = new ValidationMaxSetOfNumber(listLong);
+        if (validationI == null) {
+            if ("N".equals(System.getProperty("ACCEPT_NEG_NUMBER"))) {
+                (new ValidationDenyNegativeNumberInSet(listLong)).validate();
+            }
+            if ("Y".equals(System.getProperty("MAX_NUMBER_2"))) {
+                (new ValidationMaxSetOfNumber(listLong)).validate();
+            }
+        } else {
             validationI.validate();
         }
         Long res = Long.valueOf(listLong.stream().mapToInt(Integer::intValue).reduce(1, (a,b) -> a * b));
